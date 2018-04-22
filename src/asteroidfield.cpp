ee50977c58ab74e2f819b09a82273f4456e3accc
stfrha <mail@fredrikhoffman.se>
2018-04-22 23:36:00 +0200

    Added asteridfield files.

---
+++ src/asteroidfield.cpp
@@ -0,0 +1,100 @@
+#include "freespaceactor.h"
+#include "asteroid.h"
+#include "asteroidfield.h"
+
+using namespace oxygine;
+using namespace pugi;
+
+AsteroidField::AsteroidField(
+   Resources& gameResources,
+   Actor* parent,
+   b2World* world,
+   xml_node& objectNode) :
+   m_gameResources(&gameResources),
+   m_world(world),
+   m_spaceActor((FreeSpaceActor*)parent),
+   m_timeSinceLast(0)
+{
+   readAsteroidFieldNode(objectNode);
+   attachTo(parent);
+   spawnAsteroids();
+}
+
+void AsteroidField::readAsteroidFieldNode(xml_node& objectNode)
+{
+   m_leftTop = b2Vec2(objectNode.attribute("left").as_float(), objectNode.attribute("top").as_float());
+   m_rightBottom = b2Vec2(objectNode.attribute("right").as_float(), objectNode.attribute("bottom").as_float());
+   m_initialSpawn = objectNode.attribute("spawnInitial").as_int();
+   m_interval = 1.0f / objectNode.attribute("intensity").as_float() * 1000.0f;
+   m_fieldLifetime = objectNode.attribute("lifeTime").as_int();
+
+   addAsteroidSpawnInstruction(AsteroidSpawnInstruction(m_initialSpawn / 3, ASE_LARGE, m_leftTop, m_rightBottom ));
+   addAsteroidSpawnInstruction(AsteroidSpawnInstruction(m_initialSpawn / 3, ASE_MIDDLE, m_leftTop, m_rightBottom));
+   addAsteroidSpawnInstruction(AsteroidSpawnInstruction(m_initialSpawn / 3, ASE_SMALL, m_leftTop, m_rightBottom));
+
+}
+
+void AsteroidField::addAsteroidSpawnInstruction(AsteroidSpawnInstruction& inst)
+{
+   m_asteroidSpawnList.push_back(inst);
+}
+
+
+void AsteroidField::doUpdate(const oxygine::UpdateState& us)
+{
+   m_timeSinceLast += us.dt;
+
+   if (m_timeSinceLast >= m_interval)
+   {
+      float size = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 3.0f));
+
+      if (size < 0.5f)
+      {
+         addAsteroidSpawnInstruction(AsteroidSpawnInstruction(1, ASE_SMALL, m_leftTop, m_rightBottom));
+      }
+      else if (size < 1.5f)
+      {
+         addAsteroidSpawnInstruction(AsteroidSpawnInstruction(1, ASE_MIDDLE, m_leftTop, m_rightBottom));
+      }
+      else
+      {
+         addAsteroidSpawnInstruction(AsteroidSpawnInstruction(1, ASE_LARGE, m_leftTop, m_rightBottom));
+      }
+
+      m_timeSinceLast = 0;
+   }
+
+   if (m_spaceActor)
+   {
+      spawnAsteroids();
+   }
+}
+
+void AsteroidField::spawnAsteroids(void)
+{
+   for (auto it = m_asteroidSpawnList.begin(); it != m_asteroidSpawnList.end(); ++it)
+   {
+      for (int i = 0; i < it->m_num; i++)
+      {
+         // Randomise position within asteroid field
+         b2Vec2 pos;
+
+         pos.x = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (it->m_rightBottom.x  - it->m_leftTop.x))) + it->m_leftTop.x;
+         pos.y = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (it->m_rightBottom.y - it->m_leftTop.y))) + it->m_leftTop.y;
+         
+         spAsteroid asteroid = 
+            new Asteroid(
+               *m_gameResources, 
+               (SceneActor*)m_spaceActor,
+               m_world, 
+               pos, 
+               it->m_state, 
+               this);
+         asteroid->attachTo(m_spaceActor);
+         m_spaceActor->addBoundingBody((b2Body*)asteroid.get()->getUserData());
+      }
+   }
+
+   m_asteroidSpawnList.clear();
+}
+
---
+++ src/asteroidfield.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "oxygine-framework.h"
+#include "Box2D/Box2D.h"
+#include "compoundobject.h"
+#include "scales.h"
+#include "physdispconvert.h"
+#include "asteroid.h"
+
+class FreeSpaceActor;
+
+DECLARE_SMART(AsteroidField, spAsteroidField);
+
+class AsteroidField : public CompoundObject
+{
+private:
+   oxygine::Resources* m_gameResources;
+   oxygine::Actor* m_parent;
+   b2World* m_world;
+   FreeSpaceActor* m_spaceActor;
+   SceneActor* m_sceneActor;
+
+
+   b2Vec2 m_leftTop;
+   b2Vec2 m_rightBottom;
+
+   int m_initialSpawn;
+
+   // Time between spawned asteroids
+   float m_interval;
+
+   // The time, from the creation until the field stop spawning new asteroids
+	int m_fieldLifetime; // [ms}
+
+
+   int m_timeSinceLast;
+
+   std::vector<AsteroidSpawnInstruction>  m_asteroidSpawnList;
+   
+   void readAsteroidFieldNode(pugi::xml_node& objectNode);
+   void spawnAsteroids(void);
+   
+public:
+   AsteroidField(
+      oxygine::Resources& gameResources,
+      oxygine::Actor* parent,
+      b2World* world,
+      pugi::xml_node& objectNode);
+
+   void addAsteroidSpawnInstruction(AsteroidSpawnInstruction& inst);
+
+protected:
+	void doUpdate(const oxygine::UpdateState& us);
+
+};
