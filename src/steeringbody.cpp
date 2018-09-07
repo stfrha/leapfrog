d7825b6cbdafe9c1318ade8a07f32b669e2a8a42
Fredrik Hoffman <mail@fredrikhoffman.se>
2018-09-07 08:02:15 +0200

    Rebuild steeringbody

--- src/steeringbody.cpp
+++ src/steeringbody.cpp
@@ -1,139 +1,43 @@
-#include "steeringmanager.h"
 
-SteeringManager::SteeringManager(
-   b2Body* hostBody,
+#include "steeringbody.h"
+
+SteeringBody::SteeringBody(
+   b2Body* host,
    float maxVelocity,
    float wanderCircleDistance,
    float wanderCircleRadius,
    float wanderAngleChange
 ) :
-   m_hostBody(hostBody),
+   m_body(host),
    c_maxVelocity(maxVelocity),
    c_wanderCircleDistance(wanderCircleDistance),
    c_wanderCircleRadius(wanderCircleRadius),
    c_wanderAngleChange(wanderAngleChange)
 {
+
 }
 
-void SteeringManager::seek(b2Vec2 target, float slowingRadius = 0.0f)
+b2Vec2 SteeringBody::GetPosition()
 {
-	m_steering += doSeek(target, slowingRadius);
+   return m_body->GetPosition();
 }
 
-void SteeringManager::flee(b2Vec2 target)
+b2Vec2 SteeringBody::GetLinearVelocity()
 {
-	m_steering += doFlee(target);
+   return m_body->GetLinearVelocity();
 }
 
-void SteeringManager::wander()
+
+float SteeringBody::GetMass()
 {
-	m_steering += doWander();
+   return m_body->GetMass();
+
 }
 
-void SteeringManager::evade(b2Body* target)
+
+b2Vec2 SteeringBody::GetMaxVelocity()
 {
-	m_steering += doFlee(target->GetPosition());
+   return c_maxVelocity;
 }
 
-void SteeringManager::pursuit(b2Body* target)
-{
-	m_steering += doSeek(target->GetPosition());
-}
-
-b2Vec2 SteeringManager::doSeek(b2Vec2 target, float slowingRadius = 0.0f)
-{
-   b2Vec2 force;
-   float distance = 0.0f;
-   
-   b2Vec2 desired = target - m_hostBody->GetPosition();
-   distance = desired.Length();
-   desired.Normalize();
-   
-   if (distance < slowingRadius)
-   {
-      desired *= m_hostBody->getMaxVelocity() * distance / slowingRadius;
-   }
-   else
-   {
-      desired *= target->getMaxVelocity();
-   }
-
-   force = desired - m_hostBody->GetLinearVelocity();
-   
-   return force;   
-}
-
-b2Vec2 SteeringManager::doFlee(b2Vec2 target)
-{
-   b2Vec2 force;
-   float distance = 0.0f;
-   
-   b2Vec2 desired = m_hostBody->GetPosition() - target;
-   distance = desired.Length();
-   desired.Normalize();
-   
-   desired *= target->getMaxVelocity();
-
-   force = desired - m_hostBody->GetLinearVelocity;
-   
-   return force;   
-}
-
-b2Vec2 SteeringManager::doWander()
-{
-   // Calculate the circle center
-   b2Vec2 circleCenter;
-   circleCenter = m_hostBody->GetLinearVelocity();
-   circleCenter.Normalize();
-   circleCenter *= CIRCLE_DISTANCE;
-   //
-   // Calculate the displacement force
-   b2Vec2 displacement(0.0f, -1.0f);
-   displacement =* CIRCLE_RADIUS;
-   //
-   // Randomly change the vector direction
-   // by making it change its current angle
-   float len = displacement.Length();
-   displacement.x = cos(m_wanderAngle) * len;
-   displacement.y = sin(m_wanderAngle) * len;
-
-   //
-   // Change wanderAngle just a bit, so it
-   // won't have the same value in the
-   // next game frame.
-   m_wanderAngle += Math.random() * ANGLE_CHANGE - ANGLE_CHANGE * 0.5f;
-   //
-   // Finally calculate and return the wander force
-   b2Vec2 wanderForce;
-   wanderForce = circleCenter + displacement;
- 
-   return wanderForce;
-}
-
-b2Vec2 SteeringManager::doEvade(b2Body* target)
-{
-   b2Vec2 distance = target->GetPosition() - m_hostBody->GetPosition();
-   
-   float updatesAhead = distance.Length() / target->getMaxVelocity();
-   
-   b2Vec2 futurePosition = (target->GetPosition + target->GetLinearVelocity()) * updatesAhead;
-   return doFlee(futurePosition);
-}
-
-b2Vec2 SteeringManager::doPursuit(b2Body* target)
-{
-   b2Vec2 force;
-   b2Vec2 distance;
-   
-   distance = target->GetPosition() - m_hostBody->GetPosition();
-
-   float updatesNeeded = distance.Length() / m_hostBody->getMaxVelocity();
-
-   b2Vec2 tv = target->GetLinearVelocity();
-   tv *= updatesNeeded;
-
-   b2Vec2 targetFuturePosition = target->GetPosition() + tv;
-
-   return doSeek(targetFuturePosition);
-}
 
--- src/steeringbody.h
+++ src/steeringbody.h
@@ -3,44 +3,29 @@
 #include "Box2D/Box2D.h"
 #include "steeringinterface.h"
 
-class SteeringBody : public b2Body, ISteering
+class SteeringBody
 {
 private:
-   const float c_maxVelocity;
-   
-   b2Body(const b2BodyDef* bd, b2World* world);
-   ~b2Body();
+   const b2Vec2 c_maxVelocity;
+   const float c_wanderCircleDistance;
+   const float c_wanderCircleRadius;
+   const float c_wanderAngleChange;
 
 
-   // TODO: Rethink this!!! The SteeringBody class probably needs the b2Body as a
-   // pointer member since the constructor is private and a body normally is 
-   // created using b2World->CreateBody(def);
+public:
+   b2Body * m_body;
 
-
-
-
-	SteeringBody(
+   SteeringBody(
       b2Body* host,
-      float maxVelocity,
+      b2Vec2 maxVelocity,
       float wanderCircleDistance,
       float wanderCircleRadius,
       float wanderAngleChange
    );
-	
-	b2Vec2 doSeek(b2Vec2 target, float slowingRadius = 0.0f);
-	b2Vec2 doFlee(b2Vec2 target);
-	b2Vec2 doWander();
-	b2Vec2 doEvade(b2Body* target);
-	b2Vec2 doPursuit(b2Body* target);
-	
-public:
-	void update( void );
-	
-	void reset( void );
-	
-	void seek(b2Vec2 target, float slowingRadius = 0.0f);
-	void flee(b2Vec2 target);
-	void wander();
-	void evade(b2Body* target);
-	void pursuit(b2Body* target);
+		
+   b2Vec2 GetPosition();
+   b2Vec2 GetLinearVelocity();
+   float GetMass();
+   b2Vec2 GetMaxVelocity();
+
 }
