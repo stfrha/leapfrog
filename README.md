2f5c3cc41bee8c9cb42b4960541e53d865a20418
stfrha <mail@fredrikhoffman.se>
2019-01-27 15:06:50 +0100

    Added Global constants class and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # leapfrog
 Sandbox space adventure game
+
+The following Repositories are required:
+https://github.com/stfrha/oxygine-framework.git
+https://github.com/stfrha/SDL.git
+https://github.com/stfrha/MultiSelectTreeView.git
+https://github.com/stfrha/AdvancedZoomAndPanSample.git
+https://github.com/stfrha/MicroMvvm.git
+
---
+++ leapfrog-editor/Utilities/GlobalDataDirectory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapfrogEditor
+{
+   public static class GlobalConstants
+   {
+      public static string DataDirectory = @"..\..\..\leapfrog-game\data\";
+      public static string ImageDirectory = DataDirectory +  @"images\";
+   }
+}
