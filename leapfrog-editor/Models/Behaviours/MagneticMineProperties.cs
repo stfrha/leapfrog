d59df87ad085a85e8962b4dee05aa14b17699164
Fredrik Hoffman <mail@fredrikhoffman.se>
2019-02-13 20:05:07 +0100

    Added magnetic mine editing.

---
+++ leapfrog-editor/Models/Behaviours/MagneticMineProperties.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace LeapfrogEditor
+{
+   [Serializable]
+   public class MagneticMineProperties
+   {
+      #region Declarations
+
+      private double _magneticDistance;
+      private double _magneticForce;
+      private int _numOfParticles;
+      private double _particlesRadius;
+      private double _blastPower;
+
+      #endregion
+
+      #region Constructors
+
+      public MagneticMineProperties()
+      {
+         _magneticForce = 0;
+         _magneticForce = 0;
+         _numOfParticles = 0;
+         _particlesRadius = 0;
+         _blastPower = 0;
+      }
+
+      #endregion
+
+      #region Properties
+
+      [XmlAttribute("magneticDistance")]
+      public double MagneticDistance
+      {
+         get { return _magneticDistance; }
+         set { _magneticDistance = value; }
+      }
+
+      [XmlAttribute("magneticForce")]
+      public double MagneticForce
+      {
+         get { return _magneticForce; }
+         set { _magneticForce = value; }
+      }
+
+      [XmlAttribute("numOfParticles")]
+      public int NumOfParticles
+      {
+         get { return _numOfParticles; }
+         set { _numOfParticles = value; }
+      }
+
+      [XmlAttribute("particleRadius")]
+      public double ParticlesRadius
+      {
+         get { return _particlesRadius; }
+         set { _particlesRadius = value; }
+      }
+
+      [XmlAttribute("blastPower")]
+      public double BlastPower
+      {
+         get { return _blastPower; }
+         set { _blastPower = value; }
+      }
+
+
+      #endregion
+
+   }
+
+}
---
+++ leapfrog-editor/ViewModels/Behvaiours/MagneticMinePropertiesViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LeapfrogEditor
+{
+   class MagneticMinePropertiesViewModel : BehaviourViewModelBase
+   {
+      #region Declarations
+
+      private MagneticMineProperties ModelObject;
+
+      #endregion
+
+      #region Constructors
+
+      public MagneticMinePropertiesViewModel(
+         TreeViewViewModel treeParent, 
+         CompoundObjectViewModel parentVm, 
+         MainViewModel mainVm,
+         MagneticMineProperties modelObject,
+         bool enabled = true) :
+         base(treeParent, parentVm, mainVm, enabled)
+      {
+         ModelObject = modelObject;
+      }
+
+      #endregion
+
+      #region Properties
+
+      public MagneticMineProperties LocalModelObject
+      {
+         get { return ModelObject; }
+      }
+
+      public double MagneticDistance
+      {
+         get { return LocalModelObject.MagneticDistance; }
+         set
+         {
+            LocalModelObject.MagneticDistance = value;
+            OnPropertyChanged("MagneticDistance");
+         }
+      }
+
+      public double MagneticForce
+      {
+         get { return LocalModelObject.MagneticForce; }
+         set
+         {
+            LocalModelObject.MagneticForce = value;
+            OnPropertyChanged("MagneticForce");
+         }
+      }
+
+      public int NumOfParticles
+      {
+         get { return LocalModelObject.NumOfParticles; }
+         set
+         {
+            LocalModelObject.NumOfParticles = value;
+            OnPropertyChanged("NumOfParticles");
+         }
+      }
+
+      public double ParticlesRadius
+      {
+         get { return LocalModelObject.ParticlesRadius; }
+         set
+         {
+            LocalModelObject.ParticlesRadius = value;
+            OnPropertyChanged("ParticlesRadius");
+         }
+      }
+
+      public double BlastPower
+      {
+         get { return LocalModelObject.BlastPower; }
+         set
+         {
+            LocalModelObject.BlastPower = value;
+            OnPropertyChanged("BlastPower");
+         }
+      }
+
+      #endregion
+
+      #region private Methods
+
+      #endregion
+
+      #region protected Methods
+
+      #endregion
+
+      #region public Methods
+
+      #endregion
+   }
+}
