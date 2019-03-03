04426e5bb3707b09407dd43b39532b6e37e97f85
Fredrik Hoffman <mail@fredrikhoffman.se>
2019-03-03 20:18:45 +0100

    Added tocolleciont

---
+++ leapfrog-editor/ValueConverters/ToCollectionValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LeapfrogEditor
+{
+   class ToCollectionValueConverter : IValueConverter
+   {
+      public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+      {
+         if (value is ObjectFactoryPropertiesViewModel)
+         {
+            ObservableCollection<SystemViewModelBase> c = new ObservableCollection<SystemViewModelBase>();
+            c.Add((SystemViewModelBase)value);
+            return c;
+         }
+
+         return null;
+
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+      {
+         return DependencyProperty.UnsetValue;
+      }
+   }
+
+}
