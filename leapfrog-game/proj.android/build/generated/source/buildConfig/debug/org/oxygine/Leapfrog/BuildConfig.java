2bd071dba4557bc94aec94a833ee35e3ad704ffc
stfrha <mail@fredrikhoffman.se>
2024-06-14 23:29:10 +0200

    Game now builds for android.

---
+++ leapfrog-game/proj.android/build/generated/source/buildConfig/debug/org/oxygine/Leapfrog/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.oxygine.Leapfrog;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.oxygine.Leapfrog";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
